09fcb3771ae221d421ce775c046a28b1318a2829
Christian Koch <christian.koch@firmos.at>
2014-10-01 12:45:43 +0200

    sitemap scaling fixed?

--- fre_js/dojo_utils.js
+++ fre_js/dojo_utils.js
@@ -6662,14 +6662,14 @@ dojo.declare("FIRMOS.Sitemap", dijit.layout.BorderContainer, {
       }
     } else {
       var bb = element.entries[0].elementGFX.getTransformedBoundingBox();
-      var scaleX = bb[0].x - (bb[1].x-bb[0].x) / 2;
-      var scaleY = bb[1].y - (bb[2].y-bb[1].y) / 2;
+      var scaleX = bb[0].x + (bb[1].x-bb[0].x) / 2;
+      var scaleY = bb[1].y + (bb[2].y-bb[1].y) / 2;
     }
     element.childrenGroupMoveGFX.applyLeftTransform(dojox.gfx.matrix.scaleAt(scale,scaleX,scaleY));
     if (!zoomIn) {
       var bb = element.entries[0].elementGFX.getTransformedBoundingBox();
-      var x_diff = (dim.w / 2) - this.detailsDim.x - bb[0].x - (bb[1].x-bb[0].x) / 2;
-      var y_diff = dim.h / 2 - bb[1].y - (bb[2].y-bb[1].y) / 2;
+      var x_diff = (dim.w / 2) - this.detailsDim.x - (bb[0].x + (bb[1].x-bb[0].x) / 2);
+      var y_diff = (dim.h / 2) - (bb[1].y + (bb[2].y-bb[1].y) / 2);
       element.childrenGroupMoveGFX.applyTransform(dojox.gfx.matrix.translate(x_diff * distance,y_diff * distance));
     }
 
